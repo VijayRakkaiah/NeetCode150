7b1e082df9c58943e39b98a06c30ad59cee35f96
Vijay Rakkaiah <vijay.rakkaiah@gmail.com>
2025-12-06 22:39:30 +0530

    Arrays, Prefix/Suffix

---
+++ 006_Product of Array Except Self-238.py
@@ -0,0 +1,32 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        final = [1 for i in range(len(nums))]
+
+        prefix = 1
+        for i in range(len(nums)):
+            final[i] = prefix
+            prefix = prefix * nums[i]
+
+        suffix = 1
+        for j in range(len(nums)-1, -1, -1):
+            final[j] = suffix * final[j]
+            suffix = suffix * nums[j]
+
+        return final
+            
+
+'''
+347. Top K Frequent Elements
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+'''
